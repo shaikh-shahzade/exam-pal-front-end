f9ae987e1c6b995e16b633ccb1cd6b8fee70e741
shaikh shahzade <shahzade10@gmail.com>
2023-11-30 10:41:24 +0530

    About-us Stepper added

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-about-us',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
+  
+  isLinear = false;
 
+  constructor(private _formBuilder: FormBuilder) {}
 }
