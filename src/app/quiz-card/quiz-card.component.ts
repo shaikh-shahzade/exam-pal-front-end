27263b8994acd4387dc99e8073f49f2e726bf312
shaikh shahzade <shahzade10@gmail.com>
2023-12-21 12:12:00 +0530

    Quiz card Input

--- src/app/quiz-card/quiz-card.component.ts
+++ src/app/quiz-card/quiz-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-card',
@@ -6,10 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./quiz-card.component.css']
 })
 export class QuizCardComponent {
-
-  @Input() cardTitle:String;
-  @Input() cardSubTitle:String;
-  @Input() cardDescription:String;
-  
 
 }
