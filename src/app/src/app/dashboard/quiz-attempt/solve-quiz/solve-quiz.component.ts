8bfcc58300620e7dcbb3d27d03264832a6e2e452
shaikh shahzade <shahzade10@gmail.com>
2024-03-03 22:03:25 +0530

    solve quiz init

---
+++ src/app/src/app/dashboard/quiz-attempt/solve-quiz/solve-quiz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solve-quiz',
+  templateUrl: './solve-quiz.component.html',
+  styleUrls: ['./solve-quiz.component.css']
+})
+export class SolveQuizComponent {
+
+}
