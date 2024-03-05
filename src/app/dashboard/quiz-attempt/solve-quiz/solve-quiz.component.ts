7cd7330bc10e23cf85b8a91ba63ad09141e830ff
shaikh shahzade <shahzade10@gmail.com>
2024-03-05 22:51:24 +0530

    solve quiz changes

--- src/app/dashboard/quiz-attempt/solve-quiz/solve-quiz.component.ts
+++ src/app/dashboard/quiz-attempt/solve-quiz/solve-quiz.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-solve-quiz',
   templateUrl: './solve-quiz.component.html',
-  styleUrls: ['./solve-quiz.component.css']
+  styleUrls: ['./solve-quiz.component.css'],
+
 })
 export class SolveQuizComponent {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 
+  constructor(private _formBuilder: FormBuilder) {}
 }
