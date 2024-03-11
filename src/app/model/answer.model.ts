a733634122920365229afa1812373e38d5296abc
shaikh shahzade <shahzade10@gmail.com>
2024-03-11 21:20:22 +0530

    Answer and question model init

---
+++ src/app/model/answer.model.ts
@@ -0,0 +1,11 @@
+import { Question } from "./question.model";
+
+export class Answer
+{
+    Id:Number;
+    content:String;
+    isCorrect:Boolean;
+    choice:Number;
+    question:Question;
+	
+}
---
+++ src/app/model/question.model.ts
@@ -0,0 +1,10 @@
+import { Answer } from "./answer.model";
+import { Quiz } from "./quiz.model";
+
+export class Question{
+
+    quesid:Number;
+	content:String;
+	quiz:Quiz;
+	answers:Answer[];
+}
