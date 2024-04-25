2c8fa783a3450fb461f834f51c8eeabd134c7876
shaikh shahzade <shahzade10@gmail.com>
2024-04-25 15:36:39 +0530

    API call

--- src/app/dashboard/quiz-attempt/quiz-result/quiz-result.component.ts
+++ src/app/dashboard/quiz-attempt/quiz-result/quiz-result.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { interval } from 'rxjs';
+import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
+import { Result } from 'src/app/model/result.model';
+import { ApiUtilityService } from 'src/app/services/api-utility/api-utility.service';
 
 @Component({
   selector: 'app-quiz-result',
   templateUrl: './quiz-result.component.html',
   styleUrls: ['./quiz-result.component.css']
 })
-export class QuizResultComponent {
+export class QuizResultComponent implements AfterViewInit {
 
+  result:Result;
+
+  constructor(private route:ActivatedRoute , private apiUtility:ApiUtilityService)
+  {
+    
+  }
+  ngAfterViewInit(): void {
+    const resId = this.route.snapshot.paramMap.get("id");
+    if(resId!=null)
+    this.apiUtility.getResult(resId).subscribe((res=>{console.log(res)}))
+    
+  }
+
+  focusReviewSection(reviewQuiz:HTMLDivElement)
+  {
+    
+    setTimeout(()=>{reviewQuiz.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: 'end'
+      });} , 100)
+  }
 }
--- src/app/services/api-utility/api-utility.service.ts
+++ src/app/services/api-utility/api-utility.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Category } from 'src/app/model/category.model';
+import { Result } from 'src/app/model/result.model';
 import { AppConstants } from 'src/app/uitility/constants-helper';
 
 @Injectable({
@@ -9,6 +10,10 @@ import { AppConstants } from 'src/app/uitility/constants-helper';
 export class ApiUtilityService {
 
 
+  getResult(resId:string)
+  {
+    return this.httpClient.get<Result>(AppConstants.BASE_URL+`attempt/result/${resId}`);
+  }
   getCategories()
   {
     return this.httpClient.get<Category[]>(AppConstants.BASE_URL+"category")
