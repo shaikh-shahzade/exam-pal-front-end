8aca66764dcc960ae2e82ea946bb2c3939fb0954
shaikh shahzade <shahzade10@gmail.com>
2024-06-27 22:59:02 +0530

    design admin-dashboard and format changes

--- src/app/dashboard/admin-dashboard/show-users/show-users.component.ts
+++ src/app/dashboard/admin-dashboard/show-users/show-users.component.ts
@@ -5,16 +5,12 @@ import { ApiUtilityService } from 'src/app/services/api-utility/api-utility.serv
 @Component({
   selector: 'app-show-users',
   templateUrl: './show-users.component.html',
-  styleUrls: ['./show-users.component.css']
+  styleUrls: ['./show-users.component.css'],
 })
 export class ShowUsersComponent implements AfterViewInit {
-
-  users:User[];
-  constructor(private apiService:ApiUtilityService)
-  {}
+  users: User[];
+  constructor(private apiService: ApiUtilityService) {}
   ngAfterViewInit(): void {
-    this.apiService.getAllUsers().subscribe(val=>this.users=val);
+    this.apiService.getAllUsers().subscribe((val) => (this.users = val));
   }
-
-
 }
