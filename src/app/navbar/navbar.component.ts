f2459306e37d7236ba3ec174145b83e6e1b72360
shaikh shahzade <shahzade10@gmail.com>
2024-07-27 23:48:28 +0530

    navigation change fix

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,17 +11,19 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent  { 
 
-  userLogin!:boolean; //:Subject<boolean> = new Subject<boolean>();
+  userLogin!:boolean;
   userDetails!: UserDetails;
   userRoles:String[]|null;
   profilePath:string=`/user${ this.userDetails!=undefined&& this.userDetails.userRole.find(e=>e.role.role==="admin-user")!=undefined?"/admin":""}`;
 
 
+
+  
+
   baseUrl:string = AppConstants.IMAGE_URL;
-  constructor(private loginService:LoginService
-    )
+  constructor(private loginService:LoginService, private router:Router)
   {
-    this.loginService.loggedIn.subscribe( (userDetails)=>{
+      this.loginService.loggedIn.subscribe( (userDetails)=>{
       this.userLogin=true;
       this.userDetails=userDetails;
       loginService.userRoles=userDetails.userRole.flatMap(r=>r.role.role);
@@ -34,13 +36,12 @@ export class NavbarComponent  {
   }
   ngOnInit()
   {
-  
+    console.log(this.router.url)
 
   }
   logout()
   {
       this.loginService.logout();
-      console.log("Called")
   }
   
 }
