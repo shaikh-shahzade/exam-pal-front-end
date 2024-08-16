ea7532b498997cac9728635080e1a60518214181
shaikh shahzade <shahzade10@gmail.com>
2024-08-16 23:55:35 +0530

    Start page load Quiz

--- src/app/dashboard/quiz-attempt/start-page/start-page.component.ts
+++ src/app/dashboard/quiz-attempt/start-page/start-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { QuizAttempt } from 'src/app/model/quiz-attempt.model';
 import { ActiveQuizService } from 'src/app/services/active-quiz/active-quiz.service';
+import { QuizService } from 'src/app/services/quiz/quiz.service';
 import { SolveService } from 'src/app/services/solve/solve.service';
 
 @Component({
@@ -9,13 +11,16 @@ import { SolveService } from 'src/app/services/solve/solve.service';
   styleUrls: ['./start-page.component.css']
 })
 export class StartPageComponent implements OnInit{
-quizId:String;
+quizId:String|null;
 constructor(
   private solveService:SolveService,
-  private activeQuiz:ActiveQuizService
+  private activeQuiz:ActiveQuizService,
+  private router:Router,
+  private quizService:QuizService
 ){}
   ngOnInit(): void {
     this.quizId = `${this.activeQuiz.quiz.qid}`;
+    this.quizId = this.router.routerState.snapshot.root.paramMap.get("user")
   }
 
 
